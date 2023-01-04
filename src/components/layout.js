eeddfe6f0e884ebfdd8568bf271e95d752f06b66
ossianhempel <hemposse@hotmail.com>
2023-01-04 08:16:15 +0100

    Add dynamic images from data

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,8 @@ import {
   heading,
   navLinks,
   navLinkItem,
-  navLinkText
+  navLinkText,
+  siteTitle,
 } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
@@ -20,7 +21,7 @@ const Layout = ({ pageTitle, children }) => {
   `)
   return (
     <div className={container}>
-      <header>{data.site.siteMetadata.title}</header>
+      <header className={siteTitle}> {data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
